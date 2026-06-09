68d7ff33548fc672acad10fa86a9f1726a4f3067
kxbluescreenofdeath <kevinbernardo168@gmail.com>
2026-06-09 17:51:40 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: ">> 𝖢𝗅𝗂𝗉𝗉𝖾𝗋𝗌𝖯𝖧 𝖲𝗒𝗌 𝖠𝖨 [ALGO-MATRIX CORE] <<",
+        name: ">> 𝖢𝗅𝗂𝗉𝗉𝖾𝗋𝗌𝖯𝖧 𝖲𝗒𝗌 𝖠𝖨 <<",
         // Activity type number (0 = Playing).
         type: 0, 
       },
